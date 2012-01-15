57616a53552860ee71da38556538ef744a86e672
natematias <natematias@gmail.com>
2012-01-15 21:58:34 +0000

    Added PhoneGap

--- src/com/civicmedia/SamplePhoneGap.java
+++ src/com/civicmedia/SamplePhoneGap.java
@@ -1,15 +1,15 @@
 package com.civicmedia;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.phonegap.*;
 
-public class SamplePhoneGap extends Activity
+public class SamplePhoneGap extends DroidGap
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
