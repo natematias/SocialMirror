23b7ebbab5affe59ddf204255cfe5d2dd098b512
J. Nathan Matias <natematias@gmail.com>
2012-04-20 18:37:10 -0400

    Added basic structure for bullseye view. Kindle version

--- assets/www/app/views.js
+++ assets/www/app/views.js
@@ -1,37 +1,79 @@
-(function($){
-
-  //View ListView
-  var SplashView = Backbone.View.extend({
+//View ListView
+var SplashView = Backbone.View.extend({
      
-    events: {
-      "click #start": "startSurvey",
-      "click #demo": "startDemo",
-      "click #name_button": "saveName"
-    },
+  events: {
+    "click #start": "startSurvey",
+    "click #demo": "startDemo",
+    "click #name_button": "saveName"
+  },
 
-    initialize: function(){
-      _.bindAll(this, 'render');
-      this.render();
-    },
-     
-    render: function(){
-     //$(this.el).append("<ul><li>hello world</li></ul>");
-     $(this.el).load("templates/splash.template");
-    },
+  initialize: function(){
+    _.bindAll(this, 'render', 'loadMLGroups', 'startDemo', 'drawBullseye');
 
-    startSurvey: function(){
-     $("#initial_buttons_view").load("templates/enter_name.template");
-    },
-  
-    startDemo: function(){
-      alert("Start Demo");
-    },
+    this.participants = new Participants();
+    this.mlgroups = new MLGroups();
+    this.relationships = new ParticipantMLGroupRelationships();
+    this.loadMLGroups();
+    this.render();
+  },
+   
+  render: function(){
+   $(this.el).load("templates/splash.template");
+  },
 
-    saveName: function(){
-      alert("Save Name");
+  startSurvey: function(){
+   $("#initial_buttons_view").load("templates/enter_name.template");
+  },
+
+  startDemo: function(){
+    //$(this.el).load("templates/bullseye.template");
+    that = this;
+    $.ajax({url:"templates/bullseye.template",
+              type: "GET",
+              dataType: "text",
+              success: function(data){
+                $(that.el).html(_.template(data, {groups:that.mlgroups}));
+                that.drawBullseye();
+              }
+    });
+  },
+
+  saveName: function(){
+    alert("Save Name");
+  },
+ 
+  drawBullseye: function(){
+
+    canvas_element = document.getElementById("bullseye_canvas");
+    if(canvas_element.getContext){
+      this.canvas = canvas_element.getContext('2d');
+      midpoint = canvas_element.offsetWidth/2;
+      height = canvas_element.offsetHeight * 2 + 60;
+      bottom_offset = 60;
+      middle_offset = 0;
+      // draw inner circle
+      this.canvas.fillStyle= "rgb(255,255,230)";
+      this.canvas.fillRect(0,0,midpoint*2, height);
+      this.canvas.beginPath();  
+      this.canvas.fillStyle = "rgb(255,220,186)";
+      this.canvas.arc(midpoint,height, height*0.88,0,(Math.PI/180)*180,true)
+      this.canvas.fill();
+      this.canvas.beginPath();
+      this.canvas.fillStyle = "rgb(137,190,204)";
+      this.canvas.arc(midpoint, height, height*0.73,0,(Math.PI/180)*180,true)
+      this.canvas.fill();
     }
+  },
 
-  });
-  var splashView = new SplashView;
-  $("#frame").html(splashView.el);
-})(jQuery);
+  loadMLGroups: function(){
+    var that = this;
+    jQuery.getJSON("data/medialab_groups.json", function(data){
+       $.each(data, function(key, value){
+         group = new MLGroup({name:value});
+         that.mlgroups.add(group);
+       });
+     });
+  }
+});
+var splashView = new SplashView;
+$("#frame").html(splashView.el);
