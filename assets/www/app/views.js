49c01d70683daab03d97a2a337325857d3ee8e1c
J. Nathan Matias <natematias@gmail.com>
2012-04-20 20:54:24 -0400

    updated to prevent dragged objects to accumulate under the mouse. Still TOOD: touch dragging

--- assets/www/app/views.js
+++ assets/www/app/views.js
@@ -1,18 +1,33 @@
 //View ListView
 var SplashView = Backbone.View.extend({
      
-  events: {
-    "click #start": "startSurvey",
-    "click #demo": "startDemo",
-    "click #name_button": "saveName"
+  events: function() {
+    return MOBILE ?
+    {
+      "click #start": "startSurvey",
+      "click #demo": "startDemo",
+      "click #name_button": "saveName",
+      "touchstart .bullseye_option": 'startOptionDrag',
+      "touchmove .bullseye_option": 'continueDragging',
+      "touchend .bullseye_option": 'endOptionDrag'
+    }:{
+      "click #start": "startSurvey",
+      "click #demo": "startDemo",
+      "click #name_button": "saveName",
+      "mousedown .bullseye_option": 'startOptionDrag',
+      "mousemove .bullseye_option": 'continueDragging',
+      "mouseup .bullseye_option": 'endOptionDrag',
+      "mouseout .bullseye_option": 'continueDragging',
+    }
   },
 
   initialize: function(){
-    _.bindAll(this, 'render', 'loadMLGroups', 'startDemo', 'drawBullseye');
+    _.bindAll(this, 'render', 'loadMLGroups', 'startDemo', 'drawBullseye', 'startOptionDrag', 'continueDragging');
 
     this.participants = new Participants();
     this.mlgroups = new MLGroups();
     this.relationships = new ParticipantMLGroupRelationships();
+    this.dragging = null;
     this.loadMLGroups();
     this.render();
   },
@@ -65,6 +80,61 @@ var SplashView = Backbone.View.extend({
     }
   },
 
+  startOptionDrag: function(e){
+    ml_option = $(e.target);
+    if(this.dragging !=null){
+      return;
+    }else{
+      this.dragging = ml_option;
+    }
+    if( navigator.userAgent.match(/Android/i) ) {
+      e.preventDefault();
+      pageX = e.touch.pageX;
+      pageY = e.touch.pageY;
+    }else{
+      pageX = e.pageX;
+      pageY = e.pageY;
+    }
+ 
+    this.dragpoint_offset_x = pageX - ml_option.offset().left
+    // 12 because that is the font size
+    this.dragpoint_offset_y = pageY - ml_option.offset().top + 12;
+    ml_option.addClass("dragging");
+    ml_option.css({"left": pageX-this.dragpoint_offset_x, "top": pageY-this.dragpoint_offset_y})
+  },
+
+  continueDragging: function(e){
+    ml_option = $(e.target);
+    if(this.dragging!=null && ml_option.get(0) === this.dragging.get(0)){
+    }else{
+      return;
+    }
+    if( navigator.userAgent.match(/Android/i) ) {
+      e.preventDefault();
+      pageX = e.touch.pageX;
+      pageY = e.touch.pageY;
+    }else{
+      pageX = e.pageX;
+      pageY = e.pageY;
+    }
+    if(this.dragpoint_offset_x){
+      ml_option.css({"left": pageX-this.dragpoint_offset_x, "top": pageY-this.dragpoint_offset_y})
+    }
+  },
+
+  endOptionDrag: function(e){
+    unzoom_multiplier = 0.25;
+    ml_option = $(e.target);
+    drop_offset = ml_option.offset().left + ml_option.width()*unzoom_multiplier;
+    ml_option.css({"position":"absolute"});
+    ml_option.removeClass("dragging");
+    ml_option.css({"left":drop_offset});
+    ml_option.css({"top": ml_option.offset().top+4});// 12px font 2px padding
+    this.dragpoint_offset_x = null;
+    this.dragoint_offset_y = null;
+    this.dragging = null;
+  },
+
   loadMLGroups: function(){
     var that = this;
     jQuery.getJSON("data/medialab_groups.json", function(data){
@@ -75,5 +145,6 @@ var SplashView = Backbone.View.extend({
      });
   }
 });
+window.MOBILE = navigator.userAgent.match(/mobile/i);
 var splashView = new SplashView;
 $("#frame").html(splashView.el);
